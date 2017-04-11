5837cb60b18e2a0901e37ac9805c366be742ec82
Manikanta Reddy D <manikanta.reddy.d@outlook.com>
2017-04-11 13:36:40 +0530

    Fixed File Sharing

--- src/providers/share-service.ts
+++ src/providers/share-service.ts
@@ -28,28 +28,67 @@ export class ShareService {
     var category_str = JSON.stringify(categories);
     var hash = this.mmh3gc.getHash(category_str, 42).toString();
     console.log(hash);
-    var data = {"hash":hash, "categories":categories};
+    var data = {
+      "hash":hash, 
+      "categories":categories
+    };
     var msg = "Actions for SayIt App. Refer to sayit help."
+    this.file.checkFile(
+      this.file.dataDirectory, 
+      "sayit.data"
+    ).then(
+      (state) => {
+        if(state){
+          this.writeExistingFile(msg, data);
+        }
+        else{
+          this.writeNew(msg, data);
+        }
+      }
+    );
+  }
+
+  writeNew(msg, data){
     this.file.writeFile(
       this.file.dataDirectory, 
       "sayit.data", 
       JSON.stringify(data), 
-      true).then(
-        (update) => {
-          this.ss.share(msg, "SayIt Data", this.file.dataDirectory+"/sayit.data", "").then(
-            (update) => {
-            }
-          ).catch(
-            (err)=>{
-              console.log(err);
-            }
-          )
-        }
-      ).catch(
-        (err) => {
-          console.log(err);
-        }
+      true
+    ).then(
+      (update) => {
+        this.invokeShare(msg);
+      }
+    ).catch(
+      (err) => {
+        console.log(err);
+      }
     );
   }
+  
+  writeExistingFile(msg, data){
+    this.file.removeFile(
+      this.file.dataDirectory,
+      "sayit.data"
+    ).then(
+      ()=>{
+        this.writeNew(msg, data);
+      }
+    ).catch(
+      (er)=>{
+        console.log(er);
+      }
+    )
+  }
 
+  invokeShare(msg){
+    this.ss.share(msg, "SayIt Data", this.file.dataDirectory+"/sayit.data", "")
+    .then(
+      (update) => {
+      }
+    ).catch(
+      (err)=>{
+        console.log(err);
+      }
+    );
+  }
 }
